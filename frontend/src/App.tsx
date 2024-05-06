df361e8a79ae98931cd3a58e92c16a137061dbb2
devMorshed <ac3m0r@gmail.com>
2024-05-06 12:34:04 +0600

    FRONTEND ADDED

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="h-screen bg-cyan-200">
+      <div className="flex items-center justify-center h-full text-6xl">
+        Welcome to Hono - Bun🥖
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { testRoute } from "./routes/test";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 app.use("*", logger());
 
 app.route("/api/v1/test", testRoute);
+
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
 export default app;
